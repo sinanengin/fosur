33dd368e6b6a7ae73fc203373570d159e9e437b0
sinanengin <sinan.engin.02@gmail.com>
2025-04-03 13:52:51 +0300

    before work update

---
+++ Pages/MyVehicleDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyVehicleDetailView.swift
+//  fosur
+//
+//  Created by Sinan Engin Yıldız on 6.03.2025.
+//
+
+import SwiftUI
+
+struct MyVehicleDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyVehicleDetailView()
+}
