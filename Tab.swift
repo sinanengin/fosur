672320627a3ce35006411da996f6ddd5bf0c0a26
sinanengin <sinan.engin.02@gmail.com>
2025-05-28 14:04:43 +0300

    Telefonla giriş kısmı entegre edildi düzenlemelere ihtiyaç var. Duyurular ekranı güncellendi.

--- Tab.swift
+++
@@ -1,9 +0,0 @@
-enum Tab: String, CaseIterable, Identifiable, Hashable {
-    case home
-    case vehicles
-    case messages
-    case profile
-    case callUs
-
-    var id: String { self.rawValue }
-}
